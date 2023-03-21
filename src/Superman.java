2a1b89a2311c83a829257f5a3f2c4125bdbe5860
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-21 07:03:23 +0100

    create fly(), run(), swim()

--- src/Superman.java
+++ src/Superman.java
@@ -2,5 +2,20 @@
 //Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать" CanRun с методом
 // run и "умеет плавать" CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
 
-public class Superman {
+public class Superman implements CanFly, CanRun, CanSwim {
+
+  @Override
+  public void fly(){
+    System.out.println("Умеет летать");
+  }
+
+  @Override
+  public void run(){
+    System.out.println("Умеет бегать");
+  }
+
+  @Override
+  public void swim(){
+    System.out.println("умеет плавать");
+  }
 }
